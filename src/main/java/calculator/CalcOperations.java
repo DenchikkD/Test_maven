00d5b2357802a35003a4ec7bfff1424e8ba4d7c9
Denni <Online08.08@mail.ru>
2016-10-01 00:51:33 +0300

    commit

---
+++ src/main/java/calculator/CalcOperations.java
@@ -0,0 +1,20 @@
+package calculator;
+
+/**
+ * Created by Denni on 9/28/2016.
+ */
+public enum CalcOperations {
+
+    DIVIDE('/'),MULTIPLY('*'),ADDED('+'),SUBTRACTION('-');
+
+
+    private char operation;
+
+    CalcOperations(char operation) {
+        this.operation = operation;
+    }
+
+    public char getOperation() {
+        return operation;
+    }
+}
